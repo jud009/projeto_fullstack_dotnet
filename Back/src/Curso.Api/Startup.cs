10f9b99d453843198d62b74ee7e556f6660e6f8e
judiclei <judiclei41@gmail.com>
2022-10-18 07:01:02 -0300

    Angular configurado no projeto

--- Back/src/Curso.Api/Startup.cs
+++ Back/src/Curso.Api/Startup.cs
@@ -32,6 +32,7 @@ namespace Curso.Api
                 context => context.UseSqlite(Configuration.GetConnectionString("Default"))
             );
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Curso.Api", Version = "v1" });
@@ -53,6 +54,10 @@ namespace Curso.Api
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(
+                access => access.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()
+            );
 
             app.UseEndpoints(endpoints =>
             {
